69bdefab6b85e77c4bb7706e0893521439e0b227
Marv35 <163859687+Marv35@users.noreply.github.com>
2025-06-03 14:19:30 +0200

    Corrections des dépendances

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
             <div className="relative">
               <div className="absolute -inset-4 bg-gray-800 rounded-full opacity-20 blur-xl animate-pulse"></div>
               <Image
-                src="/images/Terrains/courts_1_2.jpg"
+                src="/images/Terrains/courts_1_2.webp"
                 alt="Terrains de tennis"
                 width={500}
                 height={500}
