f267255be6de15b1f43bda8a1269548931d990e3
Marv35 <163859687+Marv35@users.noreply.github.com>
2025-06-16 17:42:10 +0200

    Nom vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,9 +12,9 @@ const inter = Inter({ subsets: ["latin"], display: "swap" })
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://tenniscloyes.fr"),
-  title: "Association Cloysienne de Tennis (ACT) | Club de Tennis à Cloyes-les-Trois-Rivières",
+  title: "Club de Tennis de Cloyes | Association Cloysienne de Tennis (ACT)", // <-- Modifie ici le titre affiché dans Google
   description:
-      "ASSOCIATION CLOYSIENNE DE TENNIS à Cloyes-les-Trois-Rivières (28220). ACT propose des cours, stages, compétitions et location de courts. Rejoignez l'Association Cloysienne de Tennis ! Terrains extérieurs et couverts.",
+    "Club de Tennis à Cloyes-les-Trois-Rivières : entraînements, tournois, location de courts, école de tennis et passion du sport. Rejoignez l'ACT !",
   keywords:
     "tennis, cloyes tennis, tennis cloyes, cloyes tennis act, TENNIS CLOYES, CLOYES TENNIS, tenniscloyes, Tenniscloyes, Cloyestennis, Tennis Cloyes, Cloyes Tennis, Tennis 28, tennis28, Cloyes-les-Trois-Rivières, ACT, club de tennis, cours de tennis, stages tennis, compétition tennis, Eure-et-Loir, 28220, tennis cloyes, tennis cloyes sur le loir, tennis eure et loir, act cloyes, act 28, association cloysienne de tennis, location court tennis, terrains tennis, tennis extérieur, tennis couvert, tennis pour tous les niveaux, école de tennis, entraînement tennis, act28, tennis-cloyes, tennis-cloyes-les-trois-rivières, tennis-cloyes-les-trois-rivières-28, jouer au tennis a cloyes, ou jouer au tennis a cloyes, jouer au tennis a cloyes les trois rivières, jouer au tennis a cloyes les trois rivières 28, association cloisonne de tennis, association cloisonne de tennis 28, association cloisonne de tennis cloyes, association cloisonne de tennis cloyes les trois rivières, association cloisonne de tennis cloyes les trois rivières  28, comment jouer au tennisa cloyes, comment jouer au tennis a cloyes les trois rivières, comment jouer au tennis a cloyes les trois rivières 28, tennis pour tous les niveaux, école de tennis cloyes, entraînement tennis cloyes, ou jouer au tennis a cloyes les trois rivières, ou jouer au tennis a cloyes les trois rivières 28, association cloyesienne de tennis cloyes les trois rivières, association cloyesienne de tennis cloyes les trois rivières 28, club de tennis 28",
   authors: [{ name: "Association Cloysienne de Tennis" }],
