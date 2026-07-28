180671e22be6b28d9193f098fda87e26e3685c81
Marv35 <163859687+Marv35@users.noreply.github.com>
2026-07-28 17:14:11 +0200

    lien

--- components/footer.tsx
+++ components/footer.tsx
@@ -128,7 +128,12 @@ export default function Footer() {
 
         {/* Copyright */}
         <div className="text-center text-sm text-gray-200">
-          <p>&copy; {new Date().getFullYear()} Association Cloysienne de Tennis - Maël - Tous droits réservés</p>
+          <p>
+            &copy; {new Date().getFullYear()} Association Cloysienne de Tennis - <Link 
+              href="https://github.com/Marv35/" 
+              target="_blank"
+              className="hover:text-yellow-300 transition-colors underline"
+            >Marv</Link> - Tous droits réservés</p>
           <div className="mt-3 flex justify-center space-x-4">
             <Link 
               href="/mentions-legales" 
